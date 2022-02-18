b2d2297a248e842b377ec705090ed7b0fed7f38e
wuaoxiang <wuaoxiang@stargraph.cn>
2022-02-18 21:32:34 +0800

    finish lru impl, but SIGSEGV bug

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,12 @@
 pub mod prelude {
     pub use std::collections::HashMap;
+    pub use std::hash::Hash;
+    // pub use std::fmt::Debug;
     // warning: bounds on generic parameters are not enforced in type aliases
 
-    pub trait Lru<K, V> {
+    pub trait Lru<K: Default + Hash + Eq + Clone, V: Default> {
         fn new(capacity: usize) -> Self;
-        fn put(&mut self, key: K, val: V);
-        fn get(&mut self, key: K) -> Option<V>;
+        unsafe fn put(&mut self, key: K, val: V);
+        unsafe fn get(&mut self, key: K) -> Option<V>;
     }    
 }
--- src/impl_by_linked_list_with_raw_pointer.rs
+++ src/impl_by_linked_list_with_raw_pointer.rs
@@ -1,5 +1,8 @@
+use std::fmt::Debug;
+
 use crate::common::prelude::*;
 
+// #[derive(Debug)]
 struct ListNode<K, V> {
     key: K,
     val: V,
@@ -18,29 +21,96 @@ impl<K: Default, V: Default> Default for ListNode<K, V> {
     }
 }
 
-struct DoublyLinkedList<K, V> {
+struct DoublyLinkedList<K: Default, V: Default> {
     head: ListNode<K, V>,
     tail: ListNode<K, V>
 }
 
-struct LruV1<K, V> {
+struct LruImpl<K: Default, V: Default> {
     // TODO limit linkedlist capacity
     // capacity: usize
     head: ListNode<K, V>,
     tail: ListNode<K, V>,
-    cache: HashMap<K, *const ListNode<K, V>>
+    cache: HashMap<K, *mut ListNode<K, V>>
 }
 
-impl<K, V> Lru<K, V> for LruV1<K, V> {
-    fn new(capacity: usize) -> Self {
-        todo!()
+impl<K: Default, V: Default> LruImpl<K, V> {
+    unsafe fn detach_node(&mut self, node: *mut ListNode<K, V>) {
+        // detach node
+        (*(*node).next).prev = (*(*node).prev).next;
+        (*(*node).prev).next = (*(*node).next).next;
     }
 
-    fn put(&mut self, key: K, val: V) {
-        todo!()
+    /// before: head->...->node->...->tail
+    /// after : head->node->...->tail
+    unsafe fn move_node_to_head(&mut self, node: *mut ListNode<K, V>) {
+        // let a = (*(*node).prev).next;
+
+        // detach node
+        if !(*node).prev.is_null() && !(*node).next.is_null() {
+            self.detach_node(node);
+        }
+        
+        // head->next connect to node
+        (*node).next = (*self.head.next).prev;
+        (*self.head.next).prev = node;
+        // head connect to node
+        self.head.next = node;
+        (*node).prev = &mut self.head;
     }
 
-    fn get(&mut self, key: K) -> Option<V> {
-        todo!()
+    // used in debug
+    // unsafe fn first()
+
+    // debug impl
+    // unsafe fn nomalize() {}
+}
+
+impl<K: Default + Hash + Eq + Clone, V: Default + Clone> Lru<K, V> for LruImpl<K, V> {
+    fn new(_capacity: usize) -> Self {
+        let mut head = ListNode::default();
+        let mut tail = ListNode::default();
+        head.next = &mut tail;
+        tail.prev = &mut head;
+        Self {
+            head,
+            tail,
+            cache: HashMap::new()
+        }
+    }
+
+    unsafe fn put(&mut self, key: K, val: V) {
+        if let Some(node_ptr) = self.cache.get(&key) {
+            (*(*node_ptr)).val = val;
+            self.move_node_to_head(*node_ptr);
+            return;
+        }
+
+        let mut node = ListNode {
+            key,
+            val,
+            next: self.head.prev,
+            prev: &mut self.head,
+        };
+        self.cache.insert(node.key.clone(), &mut node);
+    }
+
+    unsafe fn get(&mut self, key: K) -> Option<V> {
+        // ownership problem
+        let self_ = self as *mut Self;
+        (*self_).cache.get(&key).map(|node_ptr| {
+            self.move_node_to_head(*node_ptr);
+            (*(*node_ptr)).val.clone()
+        })
+    }
+}
+
+#[test]
+fn test() {
+    let mut lru = LruImpl::<&'static str, &'static str>::new(10);
+    unsafe {
+        lru.put("key1", "val1");
+        lru.put("key2", "val2");
+        dbg!(lru.get("key1"));
     }
 }
